c9645d0083e8dd19a0cb0e6e49b80a9a9b7929fa
Waqas Ahmad Qazi <88557951+QaziWaqasAhmad@users.noreply.github.com>
2025-02-04 18:10:30 +0500

    Update formhandlernew2.js

--- formhandlernew2.js
+++ formhandlernew2.js
@@ -1,46 +1,8 @@
 (function () {
-  alert("ge")
-    function submitFormData(formElement) {
-      const formData = new FormData(formElement)
-        console.log(window.location.hostname, "host name")
-      const data = {
-        title: formElement.getAttribute("name") || "Untitled Form", 
-        pageName: window.location.pathname || '/', 
-      }
-
-      formData.forEach((value, key) => {
-        data[key] = value 
-      })
-
-      const apiUrl = `http://staging.agencyeleva.com/api/v1/forms/response?host=${window.location.hostname}`
-  
-      fetch(apiUrl, {
-        method: "POST",
-        headers: {
-          'Content-Type': "application/json",
-        },
-        body: JSON.stringify(data),
-      })
-        .then((response) => response.json())
-        .then((data) => {
-          console.log("Form submitted successfully:", data)
-          alert("Form submitted successfully")
-        })
-        .catch((error) => {
-          console.error("Error submitting form:", error)
-        })
-    }
-  
-    document.addEventListener("DOMContentLoaded", () => {
-      
-      document.querySelectorAll("form").forEach((form) => {
-        console.log(form,"form")
-        form.addEventListener("submit", (e) => {
-          e.preventDefault() 
-          submitFormData(form) 
-        })
-      })
-    })
-
-  
-  })()
+  document.addEventListener("DOMContentLoaded", function () {
+    const inputs = document.querySelectorAll("input");
+    inputs.forEach((input) => {
+      input.style.backgroundColor = "red";
+    });
+  });
+})();
