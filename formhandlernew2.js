57b256b3fc148ce67348487f8be337a194e91339
Waqas Ahmad Qazi <88557951+QaziWaqasAhmad@users.noreply.github.com>
2025-02-04 14:39:35 +0500

    Add files via upload

---
+++ formhandlernew2.js
@@ -0,0 +1,45 @@
+(function () {
+  alert("ge")
+    function submitFormData(formElement) {
+      const formData = new FormData(formElement)
+        console.log(window.location.hostname, "host name")
+      const data = {
+        title: formElement.getAttribute("name") || "Untitled Form", 
+        pageName: window.location.pathname || '/', 
+      }
+
+      formData.forEach((value, key) => {
+        data[key] = value 
+      })
+  
+      const apiUrl = `http://staging.agencyeleva.com/api/v1/forms/response?host=${window.location.hostname}`
+  
+      fetch(apiUrl, {
+        method: "POST",
+        headers: {
+          'Content-Type': "application/json",
+        },
+        body: JSON.stringify(data),
+      })
+        .then((response) => response.json())
+        .then((data) => {
+          console.log("Form submitted successfully:", data)
+          alert("Form submitted successfully")
+        })
+        .catch((error) => {
+          console.error("Error submitting form:", error)
+        })
+    }
+  
+    document.addEventListener("DOMContentLoaded", () => {
+      
+      document.querySelectorAll("form").forEach((form) => {
+        console.log(form,"fkkjfkjjfkjfkfjfkjfjkjfjkjfjfj");
+        
+        form.addEventListener("submit", (e) => {
+          e.preventDefault() 
+          submitFormData(form) 
+        })
+      })
+    })
+  })()
